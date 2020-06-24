e879c32f3fc22a97fc7b387ba578a1981bb28e17
Keerthi <Keerthi_V03@infosys.com>
2020-06-24 02:02:38 -0700

    code

--- src/main/java/com/example/Reviewdemo/WebConfig.java
+++ src/main/java/com/example/Reviewdemo/WebConfig.java
@@ -3,9 +3,11 @@ package com.example.Reviewdemo;
 import javax.servlet.Filter;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 
+@Configuration
 public class WebConfig {
 
   @Bean
